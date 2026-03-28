3431cc10aae395dd7cc9e2602bdc472867a1561d
Nelson Ubac <54073706+necho1122@users.noreply.github.com>
2026-03-28 20:25:32 +0100

    Remove Firebase configuration from firebase.ts
    Removed sensitive Firebase configuration details.

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,13 +3,7 @@ import { getFirestore, Firestore } from 'firebase/firestore';
 import { getAuth, Auth } from 'firebase/auth';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyA2DqPHizmQVhr0Vgp8Lco7No19fahsBz4',
-	authDomain: 'my-first-fb-db.firebaseapp.com',
-	projectId: 'my-first-fb-db',
-	storageBucket: 'my-first-fb-db.firebasestorage.app',
-	messagingSenderId: '496273439981',
-	appId: '1:496273439981:web:3e362be4ba60dff882e0e4',
-	measurementId: 'G-EVXSK0S3HT',
+	
 };
 
 // Inicializar Firebase (Patrón Singleton para Next.js)
